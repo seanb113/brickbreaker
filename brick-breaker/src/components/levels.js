3d3fc3ddfb44c3611b5584c719c53e48e99e7ed8
Sean Beach <sean.beach1@yahoo.com>
2020-05-24 18:37:30 -0400

    level adjust

--- brick-breaker/src/components/levels.js
+++ brick-breaker/src/components/levels.js
@@ -11,25 +11,25 @@ const getbricks = (rows, columns) =>
 export const LEVELS = [
   {
     lives: 5,
-    paddleWidth: 3.5,
+    paddleWidth: 2.5,
     speed: 1,
     bricks: getbricks(3, 6)
   },
   {
     lives: 4,
-    paddleWidth: 3,
+    paddleWidth: 2,
     speed: 1.4,
     bricks: getbricks(4, 7)
   },
   {
     lives: 3,
-    paddleWidth: 2.5,
+    paddleWidth: 1.5,
     speed: 1.8,
     bricks: getbricks(5, 8)
   },
   {
     lives: 3,
-    paddleWidth: 2,
+    paddleWidth: 1,
     speed: 2.2,
     bricks: getbricks(6, 9)
   },
