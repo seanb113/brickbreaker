d08d3b81c2340e83937e332b89c8da188f08597a
Sean Beach <sean.beach1@yahoo.com>
2020-05-25 12:53:23 -0400

    lives

--- brick-breaker/src/components/lives.js
+++ brick-breaker/src/components/lives.js
@@ -1,12 +1,12 @@
 
 import React from 'react'
-
+import { FaRegHeart } from 'react-icons/fa'
 import { getRange } from '../props'
 
 export default ({ lives, containerWidth, unit }) => {
   const width = unit * 2
   return getRange(lives).map(i => (
-    <rect
+    <FaRegHeart
       className='life'
       rx={unit / 4}
       height={unit}
