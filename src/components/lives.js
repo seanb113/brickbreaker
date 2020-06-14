32ea5ea21dda898aa12a6c4cbab758e027395319
Sean Beach <sean.beach1@yahoo.com>
2020-06-13 20:37:28 -0400

    lives

--- src/components/lives.js
+++ src/components/lives.js
@@ -7,13 +7,7 @@ export default ({ lives, containerWidth, unit }) => {
   const width = unit * 2
   return getRange(lives).map(i => (
     <FaRegHeart
-      className='life'
-      rx={unit / 4}
-      height={unit}
-      width={width}
-      y={unit}
-      x={containerWidth - unit - width * (i + 1) - (unit / 2) * i}
-      key={i}
+
     />
   ))
 }
